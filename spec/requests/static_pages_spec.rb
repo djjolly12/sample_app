a700f8fb500468d0be9ef35067ddb7491ac4a6c3
dan <dejolly12@gmail.com>
2012-12-16 10:28:24 -0700

    updating example files

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,6 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
-
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                  :text => "Ruby on Rails Tutorial Sample App | Home")
-    end
   end
 
   describe "Help page" do
